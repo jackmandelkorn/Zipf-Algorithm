f9bfc8b70267cdc5d89a61518ab91767bd450642
Jack Mandelkorn <jacman444@gmail.com>
2017-11-29 14:06:58 -0800

    Update 1.1
    its lit

---
+++ js/generator.js
@@ -0,0 +1,31 @@
+var fileInput = $('#files');
+var uploadButton = $('#upload');
+
+uploadButton.on('click', function() {
+  if (!window.FileReader) {
+      alert('Your browser is not supported');
+      return false;
+  }
+  var input = fileInput.get(0);
+
+  // Create a reader object
+  var reader = new FileReader();
+  if (input.files.length) {
+      var textFile = input.files[0];
+      // Read the file
+      reader.readAsText(textFile);
+      // When it's loaded, process it
+      $(reader).on('load', processFile);
+  } else {
+      alert('Please upload a file before continuing')
+  }
+});
+
+function processFile(e) {
+  var file = e.target.result,
+      results;
+  if (file && file.length) {
+      results = file.replace(/\t/g,"','");
+      document.write(results);
+  }
+}
